9c4123892eaef08e3eae9bda7b6c13d04dc35e73
Danny C <dchan27@ucsc.edu>
2023-05-17 19:32:38 -0700

    where is this pushing

---
+++ src/main.js
@@ -0,0 +1,23 @@
+let config = 
+{
+    type: Phaser.CANVAS,
+    render:
+    {
+        pixelArt: true // using pixelart
+    },
+    width: 320,
+    height: 240,
+    physics:
+    {
+        default: 'arcade',
+        arcade:
+        {
+            debug:true
+        }
+    },  
+    zoom: 2,
+    scene: [Overworld]
+}
+
+
+const game = new Phaser.Game(config)
